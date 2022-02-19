757a2e83e052f21d1f658a9afe3f01afe108f88e
ChondroSW <chondrosatriowibowo@gmail.com>
2022-02-20 04:15:30 +0700

    Initial Commit

---
+++ SWIFTUI-VIPER/SWIFTUI_VIPERApp.swift
@@ -0,0 +1,20 @@
+//
+//  SWIFTUI_VIPERApp.swift
+//  SWIFTUI-VIPER
+//
+//  Created by Chondro Satrio Wibowo on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_VIPERApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
